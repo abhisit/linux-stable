d9520a972277ada70f8b0148a050c1481a88ba90
Abhisit Sangjan <s.abhisit@gmail.com>
2017-08-05 11:13:46 +0700

    lmp92001: iio: include: edit license and author's email

--- include/linux/mfd/lmp92001/core.h
+++ include/linux/mfd/lmp92001/core.h
@@ -1,14 +1,21 @@
 /*
- * include/linux/mfd/lmp92001/core.h -- Core interface for TI LMP92001
+ * include/linux/mfd/lmp92001/core.h - Core interface for TI LMP92001
  *
  * Copyright 20016 Celestica Ltd.
  *
- * Author: Abhisit Sangjan <asang@celestica.com>
+ * Author: Abhisit Sangjan <s.abhisit@gmail.com>
  *
- *  This program is free software; you can redistribute  it and/or modify it
- *  under  the terms of  the GNU General  Public License as published by the
- *  Free Software Foundation;  either version 2 of the  License, or (at your
- *  option) any later version.
+ * Inspired by wm831x driver.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
  */
 
