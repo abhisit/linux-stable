e7675ac9a41656004d041c640ab274086ed3b697
Abhisit Sangjan <s.abhisit@gmail.com>
2017-08-05 16:26:41 +0700

    lmp92001: iio: add a tail comment for write/read_raw().

--- drivers/iio/dac/lmp92001-dac.c
+++ drivers/iio/dac/lmp92001-dac.c
@@ -80,6 +80,7 @@ static int lmp92001_read_raw(struct iio_dev *indio_dev,
 		break;
 	}
 
+	/* In case of no match channel info/type is return here. */
 	return -EINVAL;
 }
 
@@ -112,6 +113,7 @@ int lmp92001_write_raw(struct iio_dev *indio_dev,
 		break;
 	}
 
+	/* In case of no match channel info/type is return here. */
 	return -EINVAL;
 }
 
