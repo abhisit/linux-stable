444371ee85aee905a5c6487cb05b477b40077a49
Abhisit Sangjan <s.abhisit@gmail.com>
2017-08-06 15:19:43 +0700

    lmp92001: mfd: using tab instead of speacebar.

--- drivers/mfd/lmp92001-debug.c
+++ drivers/mfd/lmp92001-debug.c
@@ -24,44 +24,44 @@
 #include <linux/mfd/lmp92001/core.h>
 
 static ssize_t lmp92001_id_ver_show(struct device *dev,
-                                struct device_attribute *attr, char *buf)
+	struct device_attribute *attr, char *buf)
 {
-        struct lmp92001 *lmp92001 = dev_get_drvdata(dev);
-        int ret;
-        unsigned int comid, ver;
+	struct lmp92001 *lmp92001 = dev_get_drvdata(dev);
+	int ret;
+	unsigned int comid, ver;
 
-        ret = regmap_read(lmp92001->regmap, LMP92001_ID, &comid);
-        if (ret < 0) {
-                dev_err(lmp92001->dev, "failed to read Company ID: %d\n", ret);
-                return 0;
-        }
+	ret = regmap_read(lmp92001->regmap, LMP92001_ID, &comid);
+	if (ret < 0) {
+		dev_err(lmp92001->dev, "failed to read Company ID: %d\n", ret);
+		return 0;
+	}
 
-        ret = regmap_read(lmp92001->regmap, LMP92001_VER, &ver);
-        if (ret < 0) {
-                dev_err(lmp92001->dev, "failed to read Version: %d\n", ret);
-                return 0;
-        }
+	ret = regmap_read(lmp92001->regmap, LMP92001_VER, &ver);
+	if (ret < 0) {
+		dev_err(lmp92001->dev, "failed to read Version: %d\n", ret);
+		return 0;
+	}
 
-        ret = sprintf(buf, "Company ID 0x%02x (%d), Version 0x%02x (%d)\n",
-                        comid, comid, ver, ver);
+	ret = sprintf(buf, "Company ID 0x%02x (%d), Version 0x%02x (%d)\n",
+			comid, comid, ver, ver);
 
-        return ret;
+	return ret;
 }
 static DEVICE_ATTR(lmp92001_id_ver, 0444, lmp92001_id_ver_show, NULL);
 
 int lmp92001_debug_init(struct lmp92001 *lmp92001)
 {
-        int ret;
+	int ret;
 
-        ret = device_create_file(lmp92001->dev, &dev_attr_lmp92001_id_ver);
-        if (ret != 0)
-                dev_err(lmp92001->dev,
-                        "unique ID attribute is not created: %d\n", ret);
+	ret = device_create_file(lmp92001->dev, &dev_attr_lmp92001_id_ver);
+	if (ret != 0)
+		dev_err(lmp92001->dev,
+			"unique ID attribute is not created: %d\n", ret);
 
-        return ret;
+	return ret;
 }
 
 void lmp92001_debug_exit(struct lmp92001 *lmp92001)
 {
-        device_remove_file(lmp92001->dev, &dev_attr_lmp92001_id_ver);
+	device_remove_file(lmp92001->dev, &dev_attr_lmp92001_id_ver);
 }
