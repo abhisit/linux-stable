661e4780b5bdcecef0faef304dade3e448e3870b
Abhisit Sangjan <s.abhisit@gmail.com>
2017-08-23 17:14:55 +0700

    lmp92001: mfd: removed license tail line.

--- include/linux/mfd/lmp92001/core.h
+++ include/linux/mfd/lmp92001/core.h
@@ -16,7 +16,6 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
  */
 
 #ifndef __MFD_LMP92001_CORE_H__
